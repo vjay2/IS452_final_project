b5e7775f25dbbac891d4673ac1da8025a01d580d
vjay2 <35739801+vjay2@users.noreply.github.com>
2019-05-12 11:36:23 -0500

    Add files via upload

---
+++ helpSeekingCounter.py
@@ -0,0 +1,72 @@
+import csv
+import pprint
+
+rows = []
+with open('TQ_AC_data.csv', 'r', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    first_row = True
+    for row in reader:
+        if not first_row:
+            rows.append(row)
+        else:
+            first_row = False
+
+#pprint.pprint(rows)
+
+all_data = {}
+def process_row(row):
+    user_id = row[4]
+    TQ = row[6]
+    AC = row[5]
+
+    k = user_id
+    if k not in all_data.keys():
+        all_data[k] = {"TQ_0_count":0, "TQ_1_count":0, "TQ_2_count":0,
+                       "TQ_3_count":0, "AC_0_count":0, "AC_1_count":0, "AC_2_count":0,
+                       "AC_3_count":0, "count":0}
+    this_data = all_data[k]
+
+    this_data["count"] += 1
+    if TQ == "0":
+        this_data["TQ_0_count"] += 1
+    if TQ == "1":
+        this_data["TQ_1_count"] += 1
+    if TQ == "2":
+        this_data["TQ_2_count"] += 1
+    if TQ == "3":
+        this_data["TQ_3_count"] += 1
+    if AC == "0":
+        this_data["AC_0_count"] += 1
+    if AC == "1":
+        this_data["AC_1_count"] += 1
+    if AC == "2":
+        this_data["AC_2_count"] += 1
+    if AC == "3":
+        this_data["AC_3_count"] += 1
+
+for row in rows:
+    if len(row) > 0:
+        process_row(row)
+
+output_file = open("counted_file.csv", "w")
+
+output_file.write("user_id, TQ_0, TQ_1, TQ_2, TQ_3, AC_0, AC_1, AC_2, AC_3, count\n")
+
+for k in all_data.keys():
+    (user_id) = k
+    this_data = all_data[k]
+    TQ_0 = this_data["TQ_0_count"]
+    TQ_1 = this_data["TQ_1_count"]
+    TQ_2 = this_data["TQ_2_count"]
+    TQ_3 = this_data["TQ_3_count"]
+    AC_0 = this_data["AC_0_count"]
+    AC_1 = this_data["AC_1_count"]
+    AC_2 = this_data["AC_2_count"]
+    AC_3 = this_data["AC_3_count"]
+    count = this_data["count"]
+
+    output_file.write(str(user_id) + ',' + str(TQ_0) + ',' + str(TQ_1) + ',' +
+                      str(TQ_2) + ',' + str(TQ_3) + ',' + str(AC_0) + ',' +
+                      str(AC_1) + ',' + str(AC_2) + ',' + str(AC_3) + ',' +
+                      str(count) + '\n')
+output_file.close()
